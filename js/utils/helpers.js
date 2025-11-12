f2805bf818ee118e49cd26975ec115af64c0ee96
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2025-11-12 11:58:47 +0300

    Update helpers.js

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,1 +1,22 @@
+// Вспомогательные функции
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
 
+function formatPrice(price) {
+  return new Intl.NumberFormat('ru-RU').format(price);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export { debounce, formatPrice, getRandomInt };
