90147d613239059e691ab44458f568b2d8a8bc6e
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2025-11-12 11:57:51 +0300

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -1,1 +1,37 @@
+function initHeader() {
+  initMarketplaceSwitcher();
+  initCitySelector();
+  initSearch();
+}
 
+function initMarketplaceSwitcher() {
+  const switcherOptions = document.querySelectorAll('.switcher-option');
+  
+  switcherOptions.forEach(option => {
+    option.addEventListener('click', function() {
+      const target = this.getAttribute('data-target');
+      
+      switcherOptions.forEach(opt => opt.classList.remove('active'));
+      this.classList.add('active');
+      
+      if (target === 'opt') {
+        window.location.href = 'https://nyaganmaksimov-sys.github.io/ciuim-opt/';
+      }
+    });
+  });
+}
+
+function initCitySelector() {
+  const locationPicker = document.getElementById('locationPicker');
+  const cityModal = document.getElementById('cityModal');
+  
+  // ... код выбора города
+}
+
+function initSearch() {
+  const searchInput = document.querySelector('.search-bar input');
+  
+  searchInput.addEventListener('input', debounce(function(e) {
+    console.log('Поиск:', e.target.value);
+  }, 300));
+}
